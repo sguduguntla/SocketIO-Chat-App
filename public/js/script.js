15fc60096681f1e8a50de2009ed5109d694793ce
Sriharsha Guduguntla <sguduguntla11@gmail.com>
2017-07-07 12:44:44 -0700

    Improving UI of chat app

--- public/js/script.js
+++ public/js/script.js
@@ -4,7 +4,7 @@ var socket = io();
 
 console.log(name + " wants to join " + room + "!");
 
-$(".room-title").text(room);
+$(".room-title").text("Chat Room: " + room);
 
 socket.on('connect', function() {
     console.log("Connected to socket.io server!");
@@ -20,19 +20,38 @@ socket.on('message', function(message) {
     console.log('New message:');
     console.log(message.text);
     var $messages = $('.messages');
-    var $message = $("<li class='list-group-item'></li>");
+    var messageType;
+    if (message.name == name) {
+        messageType = "myMessage";
+    } else {
+        messageType = "incomingMessage";
+    }
+    var $message = $("<li class='list-group-item " + messageType + "'></li>");
     $message.append('<p><strong>' + message.name + ' ' + momentTimestamp.format('h:mm a') + ': </strong></p>');
     $message.append("<p>" + message.text + "</p>");
     $messages.append($message);
+
+    $(".list-group").scrollTop($(".list-group")[0].scrollHeight);
+
+});
+
+var $form = $('#message-form');
+
+//Takes care of pressing enter in a textarea
+
+$('textarea').keypress(function(e){
+  if(e.keyCode == 13 && !e.shiftKey) {
+   e.preventDefault();
+   $form.submit();
+  }
 });
 
 // Handles submitting of new message
-var $form = $('#message-form');
 
 $form.on('submit', function(e) {
     e.preventDefault();
 
-    var $message = $form.find('input[name=message]');
+    var $message = $form.find('textarea[name=message]');
 
     socket.emit('message', {
         name: name,
